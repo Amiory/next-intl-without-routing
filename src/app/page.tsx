793414f47a355090a0bf9611f51f2cff26fc96e6
Amiory <amiory05@gmail.com>
2025-08-10 18:23:26 +0330

    Style: - removed the invert styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
@@ -22,7 +21,7 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             {translation.rich("HeroListItem1", {
               code: (chunks) => (
-                <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+                <code className="bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
                   {chunks}
                 </code>
               ),
@@ -43,7 +42,6 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
